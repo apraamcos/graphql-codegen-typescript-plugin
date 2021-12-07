0c327209840bfd6c5039631eb5df6a0f91d30348
jiarong.gu <jiaronggu@hotmail.com>
2021-12-07 14:40:20 +1100

    simplify types

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const OperationType = {
   mutation: "mutation"
 };
 
-const VariableKind = {
-  NAMED_TYPE: "NamedType",
-  LIST_TYPE: "ListType",
-  NON_NULL_TYPE: "NonNullType"
-};
-
 const imports = [
   "import { ApolloClient } from '@apollo/client';",
   "import { MutationOptions, QueryOptions } from '@apollo/client/core/watchQueryOptions';",
@@ -23,7 +17,7 @@ export const {{name}} = async<T = any>({{inputType}}) => {
   return (
     await (input.client || __client).query<{{typeName}}Query, {{typeName}}QueryVariables>({
       query: {{typeName}}Document,
-      ...input.options{{variables}}
+      ...input.options{{inputVariables}}
     })
   ).{{dataName}};
 };
@@ -39,7 +33,7 @@ export const {{name}} = async <T = any>({{inputType}}) => {
   return (
     await (input.client || __client).mutate<{{typeName}}Mutation, {{typeName}}MutationVariables>({
       mutation: {{typeName}}Document,
-      ...input.options{{variables}}
+      ...input.options{{inputVariables}}
     })
   ).{{dataName}};
 };
@@ -49,58 +43,6 @@ const mutationInputTemplate = `input: {
   client?: ApolloClient<T>,
   options?: Omit<MutationOptions, 'mutation' | 'variables'>{{variables}} 
 }{{variableUndefined}}`;
-
-const variableTypeTemplate = `{{inputName}}{{nullable}}: {{inputType}}`;
-
-const scalars = {
-  ID: "string",
-  String: "string",
-  Boolean: "boolean",
-  Int: "number",
-  Float: "number",
-  DateTime: "string",
-  JSON: "any",
-  JSONObject: "any"
-};
-
-function clearOptional(str) {
-  const rgx = new RegExp(`^Maybe<(.*?)>$`, "i");
-  if (str.startsWith(`Maybe`)) {
-    return str.replace(rgx, "$1");
-  }
-  return str;
-}
-
-function wrapTypeNodeWithModifiers(typeNode, convert) {
-  switch (typeNode?.kind) {
-    case VariableKind.NAMED_TYPE: {
-      return `Maybe<${scalars[typeNode.name.value] || convert(`${typeNode.name.value}`)}>`;
-    }
-    case VariableKind.NON_NULL_TYPE: {
-      const innerType = wrapTypeNodeWithModifiers(typeNode.type, convert);
-      return clearOptional(innerType);
-    }
-    case VariableKind.LIST_TYPE: {
-      const innerType = wrapTypeNodeWithModifiers(typeNode.type, convert);
-      return `Maybe<Array<${innerType}> | ${innerType}>`;
-    }
-  }
-  return wrapTypeNodeWithModifiers(typeNode?.type, convert);
-}
-
-function generateVariables(inputNames) {
-  let variables = "  variables: {";
-  inputNames.forEach((inputName, i) => {
-    const nullable = `${inputName.type.kind != VariableKind.NON_NULL_TYPE ? "?" : ""}`;
-
-    variables += `\n    ${replaceTemplate(variableTypeTemplate, { ...inputName, nullable })}`;
-    if (i < inputNames.length - 1) {
-      variables += ",";
-    }
-  });
-  variables += "\n  }";
-  return variables;
-}
 
 function replaceTemplate(template, variables) {
   let replaced = template;
@@ -122,32 +64,21 @@ function plugin(schema, documents, config, info) {
         definition.selectionSet.selections.length > 1
           ? "data"
           : `data?.${definition.selectionSet.selections[0].name.value}`;
-      const inputNames = (definition.variableDefinitions ?? [])
-        .map((vd) => {
-          const inputName = vd.variable.name?.value;
-          const inputType = wrapTypeNodeWithModifiers(vd.type, convert);
-          return { inputName, inputType, type: vd.type };
-        })
-        .filter((x) => x.inputName && x.inputType);
-
-      const hasVariables = inputNames && inputNames.length > 0;
-      const variables = hasVariables ? ",\n      variables: input.variables" : "";
+      const hasVariables =
+        definition.variableDefinitions && definition.variableDefinitions.length > 0;
       const variableUndefined = hasVariables ? "" : " = {}";
-      const queryVariables = { name, typeName, dataName, variables };
+      const inputVariables = hasVariables ? ",\n      variables: input.variables" : "";
+      const queryVariables = { name, typeName, dataName, inputVariables };
 
       if (definition.operation === OperationType.query) {
-        const inputType = replaceTemplate(queryInputTemplate, {
-          variableUndefined,
-          variables: hasVariables ? `,\n${generateVariables(inputNames)}` : ""
-        });
+        const variables = hasVariables ? `,\n  variables: ${typeName}QueryVariables` : "";
+        const inputType = replaceTemplate(queryInputTemplate, { variableUndefined, variables });
         contents.push(replaceTemplate(queryTemplate, { ...queryVariables, inputType }));
       }
 
       if (definition.operation === OperationType.mutation) {
-        const inputType = replaceTemplate(mutationInputTemplate, {
-          variableUndefined,
-          variables: hasVariables ? `,\n${generateVariables(inputNames)}` : ""
-        });
+        const variables = hasVariables ? `,\n  variables: ${typeName}MutationVariables` : "";
+        const inputType = replaceTemplate(mutationInputTemplate, { variableUndefined, variables });
         contents.push(replaceTemplate(mutationTemplate, { ...queryVariables, inputType }));
       }
     }
